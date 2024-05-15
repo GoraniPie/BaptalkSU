db14b623faafb5af9afc5368656feb52c00edaea
GoraniPie <scn09050@gmail.com>
2024-05-16 01:22:33 +0900

    firebase dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,4 +45,9 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation(platform("com.google.firebase:firebase-bom:33.0.0"))
+    implementation("com.google.firebase:firebase-analytics")
+    implementation("com.google.firebase:firebase-auth-ktx")
+    implementation("com.google.firebase:firebase-firestore")
 }
