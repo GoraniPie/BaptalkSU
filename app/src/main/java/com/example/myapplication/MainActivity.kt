fd8641a229bf592b45f4ac30f0a21c561b8bab27
GoraniPie <intelpieservice@gmail.com>
2024-05-23 16:15:55 +0900

    모집글 작성 화면 연결

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.myapplication
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,5 +12,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.recruit)
+        val bt_PostNew = findViewById<Button>(R.id.bt_PostNew)
+        bt_PostNew.setOnClickListener {
+            val intent = Intent(this, PostRecruitment::class.java)
+            startActivity(intent)
+        }
     }
 }
