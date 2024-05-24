7e4eea1396c197c30aebcf8f0e39a3ede515f7b2
unknown <kb9617@gmail.com>
2024-05-24 18:38:00 +0900

    네비게이션 바 테스트

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,19 +3,53 @@ package com.example.myapplication
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.example.myapplication.databinding.RecruitBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : RecruitBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = RecruitBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(Recruit())
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+
+            when(it.itemId){
+
+                R.id.recruit -> replaceFragment(Recruit())
+                R.id.map -> replaceFragment(Map())
+                R.id.profile -> replaceFragment(Profile())
+                R.id.chat -> replaceFragment(Chat())
+
+                else ->{
+
+                }
+
+            }
+
+            true
+
+        }
+
+
+
         setContentView(R.layout.recruit)
         val bt_PostNew = findViewById<Button>(R.id.bt_PostNew)
         bt_PostNew.setOnClickListener {
             val intent = Intent(this, PostRecruitment::class.java)
             startActivity(intent)
         }
+    }
+
+    private fun replaceFragment(fragment : Fragment){
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout,fragment)
+        fragmentTransaction.commit()
     }
 }
